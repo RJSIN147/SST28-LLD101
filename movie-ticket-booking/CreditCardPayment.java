87f6ea7c8a44bb26e04668913d264f08b54faf57
RJSIN147 <rjsinha2217@gmail.com>
2026-04-06 17:09:12 +0530

    miscellaneous

--- movie-ticket-booking/CreditCardPayment.java
+++ movie-ticket-booking/CreditCardPayment.java
@@ -2,7 +2,6 @@ public class CreditCardPayment implements PaymentGateway {
 
     @Override
     public boolean processPayment(int userId, int amount) {
-        // Simulate credit card payment processing
         System.out.println("  [CREDIT CARD] Processing Rs." + amount + " for User " + userId + "...");
         System.out.println("  [CREDIT CARD] Payment successful!");
         return true;
--- movie-ticket-booking/DebitCardPayment.java
+++ movie-ticket-booking/DebitCardPayment.java
@@ -2,7 +2,6 @@ public class DebitCardPayment implements PaymentGateway {
 
     @Override
     public boolean processPayment(int userId, int amount) {
-        // Simulate debit card payment processing
         System.out.println("  [DEBIT CARD] Processing Rs." + amount + " for User " + userId + "...");
         System.out.println("  [DEBIT CARD] Payment successful!");
         return true;
